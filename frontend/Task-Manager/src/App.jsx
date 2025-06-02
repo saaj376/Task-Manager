188c544e3c0f788d47a5765fd9aed6ce6aefa04c
Saajan Varghese <157048612+gundumani376@users.noreply.github.com>
2025-06-02 23:07:52 +0530

    main

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Dashboard from "./pages/User/Dashboard";
+import SignUp from "./pages/Auth/SignUp";
+import login from "./pages/Auth/Login";
+import ManageTasks from "./pages/Admin/ManageTasks";
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<login/>} />
+          <Route path="/signUp" element={<SignUp/>} />
+
+          {/* Admin Routes */}
+          <Route element ={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/Dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 export default App;
